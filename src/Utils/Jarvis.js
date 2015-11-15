4d7bacbcd943a44f52d4cf73daa3b929c02ba2d1
Lee Gary <granaldo123@yahoo.com>
2015-11-16 00:26:08 +0800

    updated speech js

--- src/Utils/Jarvis.js
+++ src/Utils/Jarvis.js
@@ -2,32 +2,48 @@
 var $                 = require('jquery'),
     Say               = require('../components/Speech');
 
-var Jarvis = (function (text){
+var Jarvis = (function (){
   var instance = {};
   
   instance.introduction = function(name) {
+    console.log('Hi Gary');
     Say('Hi ' + name + ', nice to meet you');
     //todo: store user's name..
   };
 
-  // instance.addCommands = function (text, func) {
-  //   var command = text[func];
-  //   annyang.addCommands(command);
-  //   console.debug(command);
-  // }
+  instance.addCommands = function (text, func) {
+    var command = {};
+    command[text] = func;
+    annyang.addCommands(command);
+    console.debug(command);
+  }
 
   var _init = function () {
     if(annyang) {
-      annyang.addCommands('(My name is) (I am) *name', instance.introduction);
+      instance.addCommands('i am *name', instance.introduction);
       annyang.debug(true);
       annyang.start();
     }
-  }  
+  }
 
   instance.init = function () {
     _init();
     Say('Greetings, how may i address you?');
   };
+
+  instance.pause = function () {
+    annyang.pause();
+  }
+
+  instance.resume = function () {
+    annyang.resume();
+  }
+
+  instance.shutUp = function () {
+    speechSynthesis.cancel();
+  }
+
+  
   return instance;
 })();
 
--- src/components/Speech.js
+++ src/components/Speech.js
@@ -1,61 +1,85 @@
 'use strict';
 
+var Jarvis = require('../Utils/Jarvis');
+
+//work around to load voices..
+var voices = [];
+var watch = setInterval(function() {
+   voices = speechSynthesis.getVoices();
+   if (voices.length !== 0) {     
+      clearInterval(watch);
+   }
+}, 1);
+
 var Say = function (text){
-  var utterance = new SpeechSynthesisUtterance();
-  utterance.text = text;
-  speechUtteranceChunker(utterance, {
-    chunkLength: 120
-  }, function () {
-      console.log('done');
-  });
+  if(voices.length == 0) {
+    setTimeout(function(text) {
+      Say(text);
+    }, 20, text);
+  } else {
+    var utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    utterance.voice = voices.filter(function(voice)  {
+      return voice.name == 'Daniel'; 
+    })[0];
+    annyang.pause();
+    speechUtteranceChunker(utterance, {
+      chunkLength: 120
+    }, function () {
+      annyang.resume();
+    });
+  }
 }
 
 //https://gist.github.com/woollsta/2d146f13878a301b36d7#file-chunkify-js
 
 var speechUtteranceChunker = function (utt, settings, callback) {
-    settings = settings || {};
-    var newUtt;
-    var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
-   
-    var chunkLength = (settings && settings.chunkLength) || 160;
-    var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
-    var chunkArr = txt.match(pattRegex);
+  settings = settings || {};
+  var newUtt;
+  var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
+  if(settings.cancel === true) {
+    return;
+  }
+  var chunkLength = (settings && settings.chunkLength) || 160;
+  var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
+  var chunkArr = txt.match(pattRegex);
 
-    if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
-        //call once all text has been spoken...
-        if (callback !== undefined) {
-            callback();
-        }
-        return;
-    }
-    var chunk = chunkArr[0];
-    newUtt = new SpeechSynthesisUtterance();
-    newUtt.text = chunk;
-    newUtt.voice = utt.voice;
-    var x;
-    for (x in utt) {
-        if (utt.hasOwnProperty(x) && x !== 'text') {
-            newUtt[x] = utt[x];
-        }
-    }
-    newUtt.addEventListener('end', function () {
-        if (speechUtteranceChunker.cancel) {
-            speechUtteranceChunker.cancel = false;
-            return;
-        }
-        settings.offset = settings.offset || 0;
-        settings.offset += chunk.length - 1;
-        speechUtteranceChunker(utt, settings, callback);
-    });
+  if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
+      //call once all text has been spoken...
+      if (callback !== undefined) {
+          callback();
+      }
+      return;
+  }
+  var chunk = chunkArr[0];
+  newUtt = new SpeechSynthesisUtterance();
+  newUtt.text = chunk;
+  newUtt.lang = 'en-US';
+  newUtt.voice = utt.voice;
+  var x;
+  for (x in utt) {
+      if (utt.hasOwnProperty(x) && x !== 'text') {
+          newUtt[x] = utt[x];
+      }
+  }
+  newUtt.addEventListener('end', function () {
+      if (speechUtteranceChunker.cancel) {
+          speechUtteranceChunker.cancel = true;
+          return;
+      }
+      settings.offset = settings.offset || 0;
+      settings.offset += chunk.length - 1;
+      speechUtteranceChunker(utt, settings, callback);
+  });
 
-    if (settings.modifier) {
-        settings.modifier(newUtt);
-    }
-    console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
-    //placing the speak invocation inside a callback fixes ordering and onend issues.
-    setTimeout(function () {
-        speechSynthesis.speak(newUtt);
-    }, 0);
+  if (settings.modifier) {
+      settings.modifier(newUtt);
+  }
+  console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
+  //placing the speak invocation inside a callback fixes ordering and onend issues.
+  setTimeout(function () {
+      speechSynthesis.speak(newUtt);
+  }, 0);
 };
 
 module.exports = Say;
